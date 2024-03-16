872876cca6a9e4fb794bdf11819ae41c75ba7127
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-03-17 03:56:15 +0600

    recipe & recipes added

--- src/components/recipe/Recipe.jsx
+++ src/components/recipe/Recipe.jsx
@@ -1,18 +1,18 @@
 import { IoTimeOutline } from "react-icons/io5"
 import { AiOutlineFire } from "react-icons/ai"
 import PropTypes from "prop-types"
-const Recipe = ({ recipe }) => {
+const Recipe = ({ recipe, handleWantToCook }) => {
 	const { recipe_image, recipe_name, short_description, ingredients, preparing_time, calories } = recipe
 	return (
-		<div className='w-[370px] flex flex-col p-6 border-[1px] border-solid border-[#28282833] rounded-2xl'>
-			<div className='w-[330px] h-[200px]'>
+		<div className='sm:w-[370px] flex flex-col p-6 border-[1px] border-solid border-[#28282833] rounded-2xl'>
+			<div className='sm:w-[330px] h-[200px]'>
 				<img className='w-full h-full object-cover rounded-2xl' src={recipe_image} alt='' />
 			</div>
 
-			<h4 className='text-xl font-semibold leading-6 my-4'>{recipe_name}</h4>
+			<h4 className='text-lg sm:text-xl font-semibold leading-6 my-4'>{recipe_name}</h4>
 			<p className='leading-7 mb-8 min-h-[80px]'>{short_description}</p>
-			<h4 className='text-xl font-semibold leading-6 my-4'>Ingredients:{ingredients.length}</h4>
-			<ul className='list-disc pl-6 my-2'>
+			<h4 className='text-lg sm:text-xl font-semibold leading-6 my-4'>Ingredients:{ingredients.length}</h4>
+			<ul className='list-disc pl-6 my-2 min-h-[170px]'>
 				{ingredients.map((ingredient, index) => (
 					<li key={index} className='text-lg text-[#878787] leading-8'>
 						{ingredient}
@@ -31,7 +31,9 @@ const Recipe = ({ recipe }) => {
 				</div>
 			</div>
 
-			<button className='btn bg-[#0BE58A] rounded-[50px] text-black border-none w-[170px] mt-8 text-lg font-medium leading-6'>
+			<button
+				onClick={() => handleWantToCook(recipe)}
+				className='btn bg-[#0BE58A] rounded-[50px] text-black border-none w-[170px] mt-8 text-lg font-medium leading-6'>
 				Want to Cook
 			</button>
 		</div>
@@ -40,6 +42,7 @@ const Recipe = ({ recipe }) => {
 
 Recipe.propTypes = {
 	recipe: PropTypes.object.isRequired,
+	handleWantToCook: PropTypes.func.isRequired,
 }
 
 export default Recipe
--- src/components/recipes/Recipes.jsx
+++ src/components/recipes/Recipes.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react"
 import { useEffect } from "react"
 import Recipe from "../recipe/Recipe"
+import WantToCook from "../Cook/WantToCook"
+import CurrentlyCooking from "../Cook/CurrentlyCooking"
+import { toast } from "react-toastify"
 
 const Recipes = () => {
 	const [singleRecipe, setSingleRecipe] = useState([])
+	const [wantToCook, setWantToCook] = useState([])
+	const [currentlyCooking, setCurrentlyCooking] = useState([])
 	useEffect(() => {
 		fetch("../../../public/recipe.json")
 			.then((res) => res.json())
@@ -12,19 +17,43 @@ const Recipes = () => {
 			})
 	}, [])
 
+	const handleWantToCook = (recipe) => {
+		const isAdded = wantToCook.find((item) => item.recipe_id == recipe.recipe_id)
+
+		if (!isAdded) {
+			setWantToCook([...wantToCook, recipe])
+			toast.success("Successfully added to wantToCook")
+		} else {
+			toast.error("already added in wantToCook")
+		}
+	}
+
+	const handleDelete = (id) => {
+		const newCart = wantToCook.filter((item) => item.recipe_id != id)
+		const deletedCart = wantToCook.find((item) => item.recipe_id == id)
+		setWantToCook(newCart)
+		setCurrentlyCooking([...currentlyCooking, deletedCart])
+		toast.success("Successfully added to currentlyCooking")
+	}
+
 	return (
 		<>
-			<div className='flex flex-col mt-20'>
-				<h3 className='text-4xl text-black font-semibold leading-[50px] text-center'>Our Recipes</h3>
+			<div className='flex flex-col mt-20 mx-4 sm:mx-0'>
+				<h3 className='text-2xl sm:text-4xl text-black font-semibold leading-[50px] text-center'>Our Recipes</h3>
 				<p className='leading-6 text-[#150B2B99] text-center self-center max-w-[820px] my-8'>
 					Here are some famous recipes of ours. These recipes are famous all over the world. You can see them all. They are given below.
 				</p>
 
-				<div className='flex gap-8'>
-					<div className='grid grid-cols-2 gap-8'>
+				<div className='flex flex-col lg:flex-row lg:justify-between gap-4 items-center justify-center lg:items-start'>
+					<div className='grid grid-cols-1 xl:grid-cols-2 gap-8'>
 						{singleRecipe.map((recipe, index) => (
-							<Recipe key={index} recipe={recipe} />
+							<Recipe key={index} recipe={recipe} handleWantToCook={handleWantToCook} />
 						))}
+					</div>
+
+					<div className='lg:w-[600px] h-full border-[1px] border-solid border-[#28282833] rounded-2xl flex flex-col mb-20'>
+						<WantToCook wantToCook={wantToCook} handleDelete={handleDelete} />
+						<CurrentlyCooking currentlyCooking={currentlyCooking} />
 					</div>
 				</div>
 			</div>
