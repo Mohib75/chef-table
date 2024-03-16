9e5b285aa1067534d8ab888ec0637bce86e11792
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-03-17 03:55:48 +0600

    header added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,13 @@
 const Header = () => {
 	return (
-		<div className="h-full lg:h-[600px] rounded-3xl bg-[url('../../../public/Images/banner.png')] bg-no-repeat bg-cover relative flex flex-col justify-center items-center">
-			<h2 className='text-white text-5xl font-bold leading-[76px] text-center'>
+		<div className="h-full lg:h-[600px] rounded-3xl bg-[url('../../../public/Images/banner.png')] bg-no-repeat bg-cover flex flex-col justify-center items-center p-6 mx-4 sm:mx-0">
+			<h2 className='text-white text-xl lg:text-5xl font-bold leading-10 lg:leading-[76px] text-center'>
 				Discover an exceptional cooking <br /> class tailored for you!
 			</h2>
-			<p className='text-lg leading-7 text-white my-6 w-[890px] text-center'>
+			<p className='lg:text-lg leading-7 text-white my-6 xl:w-[890px] text-center'>
 				Here you can learn every recipes from the world. We are so much eager to teach you guys how to cook any recipes.
 			</p>
-			<div className='flex gap-12 items-center'>
+			<div className='flex flex-col sm:flex-row gap-12 items-center'>
 				<button className='btn bg-[#0BE58A] rounded-[50px] text-black border-none'>Explore Now</button>
 				<button className='btn bg-transparent border-[1px] border-solid border-white text-white rounded-[50px]'>Our Feedback</button>
 			</div>
